34ead164f4a0a477081f4f4aeb60079f68adfa42
Mimi Doan <md3543@nyu.edu>
2017-11-09 00:02:41 -0500

    Adding to db

--- app.js
+++ app.js
@@ -1,52 +1,102 @@
-const express = require("express"),
-  hbs = require("hbs"),
-  mongoose = require("mongoose");
+/***************************************************************************
+*           DEPLOYED: https://ourbookmarksproject.herokuapp.com/           *
+****************************************************************************/
+
+/* ======================================================
+    Dependencies
+====================================================== */
+
+const express = require('express'),
+  hbs = require('hbs'),
+  mongoose = require('mongoose'),
+  db = require('./db.js');
+
 
 const app = express();
 
+// Set Scaffolding to hbs
 app.set('view engine', 'hbs');
 
+// register Mongoose models in app.js
+const Bookmark = mongoose.model('Bookmark');
+
+/* ======================================================
+    Middle Ware
+====================================================== */
+
+// Link All Files In Public Directory
 app.use(express.static('public'))
 
-app.get("/", (req, res) => {
-  res.redirect("/home");
+// Express Body Parser
+app.use(express.urlencoded({ extended: false }));
+
+
+
+
+
+
+/* ======================================================
+    Route Handlers
+====================================================== */
+app.get('/', (req, res) => {
+  res.redirect('/home');
 });
 
-app.get("/home", (req, res) => {
-  res.render("index");
+app.get('/home', (req, res) => {
+  res.render('index');
 });
 
-app.get("/login", (req, res) => {
-  res.render("login");
+app.get('/login', (req, res) => {
+  res.render('login');
 });
 
-app.get("/register", (req, res) => {
-  res.render("register");
+app.get('/register', (req, res) => {
+  res.render('register');
 });
 
-// app.post("/register", (req, res) => {
+// app.post('/register', (req, res) => {
 //   // issue a redirect after registration
 //   // redirect to a confirmation page or straight to feed
-//   res.redirect("register");
+//   res.redirect('register');
 // });
 
-app.get("/feed", (req, res) => {
-  res.render("feed");
+app.get('/feed', (req, res) => {
+  res.render('feed');
 });
 
-app.get("/confirm", (req, res) => {
-  res.render("confirm");
+app.get('/confirm', (req, res) => {
+  res.render('confirm');
 });
 
-app.get("/user", (req, res) => {
-  res.render("user");
+app.get('/user', (req, res) => {
+  res.render('user');
 });
 
-app.get("/create", (req, res) => {
-  res.render("create");
+/*================================================
+  display first bookmark!
+=================================================*/
+
+app.get('/create', (req, res) => {
+
+  Bookmark.find(function(err, each, count) {
+    res.render('create', {
+      Bookmark: each
+    });
+  });
+});
+
+app.post('/create', (req, res) => {
+  if(req.body['url']) {
+    const newBookmark = new Bookmark({
+      url: req.body.url,
+      name: req.body.name
+    }).save(function(err, newSave, count){
+        res.redirect('/create');
+    });
+  }
 });
 
 
 
 // will work locally and bind to port on heroku
-app.listen(process.env.PORT || 5000);
+app.listen(process.env.PORT || 3000);
--- db.js
+++ db.js
@@ -1,33 +1,44 @@
 // 1ST DRAFT DATA MODEL
 const mongoose = require('mongoose');
+const URLSlugs = require('mongoose-url-slugs');
 
 // users
 // * ues Passport to authenticate users: username and password
 // * the bookmark folders associate with their names
-const User = new mongoose.Schema({
-  // username provided by authentication plugin
-  // password hash provided by authentication plugin
-  lists:  [{ type: mongoose.Schema.Types.ObjectId, ref: 'Folder' }]
-});
+// const User = new mongoose.Schema({
+//   // username provided by authentication plugin
+//   // password hash provided by authentication plugin
+//   lists:  [{ type: mongoose.Schema.Types.ObjectId, ref: 'Folder' }]
+// });
 
 // group of Bookmarks are a folder (can also be single Bookmark)
 // * Name of the Bookmark and the associated URL
 //
 const Bookmark = new mongoose.Schema({
   url: {type: String, required: true},
-  name: {type: String, required: true},
-  quantity: {type: Number, min: 1, required: true},
-}, {
-  _id: true
+  name: {type: String, required: true}
+  // folder: {type: mongoose.Schema.Types.ObjectId, ref:'Folder'},
+  // createdAt: {type: Date, required: true},
+  // quantity: {type: Number, min: 1, required: true}, /* probably doesn't make sense, what was I thinking?? */
 });
 
 // Categorization of bookmarks into Folder
 // * each Folder must have a related user
 // * a Folder can have 0 or more items
-const Folder = new mongoose.Schema({
-  user: {type: mongoose.Schema.Types.ObjectId, ref:'User'},
-  name: {type: String, required: true},
-  createdAt: {type: Date, required: true},
-  lastUpdate: {type: Date, required: true},
-  bookmarks: [Bookmark]
-});
+// const Folder = new mongoose.Schema({
+//   user: {type: mongoose.Schema.Types.ObjectId, ref:'User'},
+//   name: {type: String, required: true},
+//   createdAt: {type: Date, required: true},
+//   lastUpdate: {type: Date, required: true},
+//   bookmarks: [Bookmark]
+// });
+
+Bookmark.plugin(URLSlugs('name'));
+
+// register models w/ mongoose!
+mongoose.model('Bookmark', Bookmark);
+// mongoose.model('Folder', Folder);
+// mongoose.model('User', User);
+
+// connection string to local instance of MongoDBa
+mongoose.connect('mongodb://localhost/bookmarks');
